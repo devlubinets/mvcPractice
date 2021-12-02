3e48fef7ec65fa914d6d425043d9b24e1e0c3aed
Lybunets Kirill <mr.lubinets@gmail.com>
2021-12-02 08:54:40 +0200

    Обновил контроллер для работы с моделью и выводом ее информации

--- project/controllers/TestController.php
+++ project/controllers/TestController.php
@@ -4,12 +4,16 @@ namespace Project\Controllers;
 
 use Core\Controller;
 use Core\Page;
+use Project\Models\Test;
 
 class TestController extends Controller
 {
     public function index():Page
     {
+        $model = Test::getModel();
+        $model->getConnection();
+        $data = $model->getById(1);
         echo '<br>'."Hi I am <b>" . __CLASS__ . "</b> and action's name <b>" . __METHOD__.'</b>';
-        return $this->render('test',['test_var' => 'test var'],'default','test title');
+        return $this->render('test',$data,'default','test title');
     }
 }
