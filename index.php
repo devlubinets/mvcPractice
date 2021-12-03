d9f43166f0c030c1fe12a873c9752fb44f495aac
Lybunets Kirill <mr.lubinets@gmail.com>
2021-12-03 09:30:34 +0200

    Перенес с index.php код в класс Core для обеспечения удобного изображения на UML диаграмме классов

--- index.php
+++ index.php
@@ -5,12 +5,5 @@ namespace Core;
 require_once 'vendor/autoload.php';
 require_once 'project/config/connection.php';
 
-$routes = require $_SERVER['DOCUMENT_ROOT'] . '/project/config/routes.php';
-
-
-$track = (new Router)->getTrack($routes, $_SERVER['REQUEST_URI']);
-$page = (new Dispatcher)->getPage($track);
-
-$str =  (new View) -> render($page);
-
-echo $str;
+$core = new Core;
+$core->run();
